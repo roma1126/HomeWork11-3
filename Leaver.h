cab42006602c5b3eed86d069de00a5ba22b95723
Roman <romka200211@gmail.com>
2023-05-23 19:09:17 +0200

    first commit

---
+++ Leaver.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class Leaver
+{
+public:
+
+	 std::string leave(std::string name);
+
+};
---
+++ LeaverClient.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Leaver.h"
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	system("chcp 1251");
+
+	std::string name;
+	Leaver func;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << func.leave(name);
+
+}
+
+
